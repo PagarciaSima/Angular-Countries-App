047d99f86a748c4d5794ece1948778c06cd796ac
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-18 16:47:43 +0200

    Country table component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 @NgModule({
   declarations: [
     ByCapitalPageComponent,
     ByCountryPageComponent,
-    ByRegionPageComponent
+    ByRegionPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
