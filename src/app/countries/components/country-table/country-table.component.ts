52f2d0129377e5720ee42a8c30ead0ad0fd39bc0
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-19 11:26:27 +0200

    Agregar paginación

--- src/app/countries/components/country-table/country-table.component.ts
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -10,4 +10,25 @@ export class CountryTableComponent {
 
   @Input()
   public countries: Country[] = [];
+
+  public currentPage: number = 1; // Página actual
+  public countriesPerPage: number = 10; // Número de países por página
+
+  // Método para obtener los países para la página actual
+  get paginatedCountries(): Country[] {
+    const startIndex = (this.currentPage - 1) * this.countriesPerPage;
+    return this.countries.slice(startIndex, startIndex + this.countriesPerPage);
+  }
+
+  // Método para cambiar de página
+  public changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+    }
+  }
+
+  // Método para obtener el número total de páginas
+  get totalPages(): number {
+    return Math.ceil(this.countries.length / this.countriesPerPage);
+  }
 }
