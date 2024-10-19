f2c21593cfbd1feecc478ae5b865b246ab9ce267
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-19 10:51:25 +0200

    Detalle del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountryService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styleUrls: ['./country-page.component.css']
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -24,8 +27,9 @@ export class CountryPageComponent implements OnInit {
         switchMap( ({id}) => this.countryService.searchCountryByAlphaCode(id))
       )
       .subscribe( country => {
-        if(!country)  return this.router.navigateByUrl('');
-        return console.log ('Tenemos un país');
+        if(!country)
+          return this.router.navigateByUrl('');
+        return this.country = country;
       });
   }
 }
